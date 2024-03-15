9323344668c1e4f6bc62de38069ccd865654754e
Nandiniranga <127603735+Nandiniranga@users.noreply.github.com>
2024-03-15 22:31:31 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        tensor : ["Tenor Sans", "sans-serif"],
+        garam : ["EB Garamond", "serif"],
+        ptserif : ["PT Serif", "serif"]
+      }
+    },
+  },
+  plugins: [],
+}
